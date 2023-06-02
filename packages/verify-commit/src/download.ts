00cf5272121710c83f7d9bc7ed3ef3d894965226
yaoxfly <1315892088@qq.com>
2023-06-02 16:17:39 +0800

    feat: @yaoxfly/verify-commit 集成husky、lint-staged、@commitlint/cli、commitizen、cz-customizable

--- packages/verify-commit/src/download.ts
+++ packages/verify-commit/src/download.ts
@@ -1,26 +1,34 @@
-import { shellExec } from '@yaoxfly/node-utils'
-import { existsSync, writeFile, readFileSync } from 'fs'
-import { resolve } from './utils.js'
+import { shellExec, readFile } from '@yaoxfly/node-utils'
+import { existsSync, writeFile, readFileSync, writeFileSync } from 'fs'
+import { resolve, packageManager } from './utils.js'
 export const downLoad = async (): Promise<void> => {
+  await husky()
+  await lintStaged()
+  await commitlint()
+  await commitizen()
+}
+
+/** @description  Git hooks
+ * @author yx
+ */
+const husky = async (): Promise<void> => {
   await shellExec({
     directive: 'npx husky-init',
     cwd: '',
-    loading: 'Downloading Husky ...',
+    loading: 'Downloading husky ...',
     log: '',
-    succeed: 'successful'
+    succeed: 'Downloading husky successful'
   })
 
-  // const content = `command_exists () {
-  //   command -v "$1" >/dev/null 2>&1
-  // }
-
-  // # Workaround for Windows 10, Git Bash and Yarn
-  // if command_exists winpty && test -t 1; then
-  //   exec < /dev/tty
-  // fi`
-  const content = readFileSync(resolve('../../sh/common.sh'))
+  const common = readFileSync(resolve('../../husky/common.sh'))
+  const preCommit = readFileSync(resolve('../../husky/pre-commit'))
   if (existsSync('.husky')) {
-    writeFile('./.husky/common.sh', content, 'utf8', (err) => {
+    writeFile('./.husky/common.sh', common, 'utf8', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+    writeFile('./.husky/pre-commit', preCommit, 'utf8', (err) => {
       if (err) {
         console.log(err)
       }
@@ -28,4 +36,104 @@ export const downLoad = async (): Promise<void> => {
   } else {
     console.log('.husky not Exist！')
   }
+}
+
+/** @description  Perform code quality verification on the code in the temporary storage area
+ * @author yx
+ */
+const lintStaged = async (): Promise<void> => {
+  const { get, directive, hasWorkspace } = packageManager()
+  await shellExec({
+    directive:directive('lint-staged', '-D'),
+    cwd: '',
+    loading: 'Downloading lint-staged ...',
+    log: '',
+    succeed: 'Downloading lint-staged successful'
+  }).then(() => {
+    const packageJsonData = readFile('package.json')
+    Object.assign(packageJsonData, {
+      'lint-staged': {
+        '*.{vue,js,ts,jsx,tsx}': [
+          hasWorkspace().value ? 'pnpm --parallel lint' : `${get()} run lint`
+        ]
+      },
+      husky: {
+        hooks: {
+          'pre-commit': 'lint-staged'
+        }
+      }
+    })
+    writeFileSync('package.json', JSON.stringify(packageJsonData, null, 2))
+  })
+}
+
+/** @description  commit content verification
+ * @author yx
+ */
+const commitlint = async (): Promise<void> => {
+  const { directive } = packageManager()
+  await shellExec({
+    directive:directive('@commitlint/cli @commitlint/config-conventional', '-D'),
+    cwd: '',
+    loading: 'Downloading commitlint ...',
+    log: '',
+    succeed: 'Downloading commitlint successful'
+  })
+
+  const file = '.commitlintrc.json'
+  const content = readFileSync(resolve('../../commitlint/.commitlintrc.json'))
+  writeFile(file, content, 'utf8', (err) => {
+    if (err) {
+      console.log(err)
+    }
+  })
+
+  const commitMsg = readFileSync(resolve('../../husky/commit-msg'))
+  if (existsSync('.husky')) {
+    writeFile('./.husky/commit-msg', commitMsg, 'utf8', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  } else {
+    console.log('.husky not Exist！')
+  }
+}
+
+/** @description  commit content verification
+ * @author yx
+ */
+const commitizen = async (): Promise<void> => {
+  const { directive } = packageManager()
+  // @commitlint/cz-commitlint
+  await shellExec({
+    directive:directive('commitizen cz-customizable commitlint-config-cz', '-D'),
+    cwd: '',
+    loading: 'Downloading commitizen ...',
+    log: '',
+    succeed: 'Downloading commitizen successful'
+  }).then(() => {
+    const packageJsonData = readFile('package.json')
+    Object.assign(packageJsonData.scripts, {
+      commit: 'git-cz'
+    })
+    const file = '.cz.config.json'
+    Object.assign(packageJsonData, {
+      config: {
+        commitizen: {
+          path: 'node_modules/cz-customizable'
+        },
+        'cz-customizable': {
+          config: file
+        }
+      }
+    })
+    writeFileSync('package.json', JSON.stringify(packageJsonData, null, 2))
+    const content = readFileSync(resolve(`../../commitlint/${file}`))
+    writeFile(file, content, 'utf8', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    })
+  })
 }
--- packages/verify-commit/src/utils.ts
+++ packages/verify-commit/src/utils.ts
@@ -1,5 +1,53 @@
 import { fileURLToPath } from 'url'
 import path from 'path'
+import { existsSync } from 'fs'
 const filename = fileURLToPath(import.meta.url)
 export const dirname = path.dirname(filename)
 export const resolve = (...file): string => path.resolve(dirname, ...file)
+
+interface PackageManager {
+  get: () => string
+  directive: (plugins: string, position: string) => string
+  hasWorkspace: () => { value: boolean, type: string }
+}
+
+/** @description According to the package manager, get the command to download the package
+   * @author yx
+   * @example
+   *  const { directive } =packageManager()
+   *  directive('lint-staged', '-D'),
+ */
+
+export const packageManager = (): PackageManager => {
+  const get = (): string => {
+    let packageManager = 'npm'
+    if (existsSync('yarn.lock')) {
+      packageManager = 'yarn'
+    }
+    if (existsSync('pnpm-lock.yaml')) {
+      packageManager = 'pnpm'
+    }
+    return packageManager
+  }
+
+  const hasWorkspace = (): { value: boolean, type: string } => {
+    let hasWorkspace = { value:false, type:'' }
+    if (existsSync('pnpm-workspace.yaml')) {
+      hasWorkspace = { value:true, type:'pnpm' }
+    }
+    return hasWorkspace
+  }
+
+  const directive = (plugins: string, position: string): string => {
+    let directive = `npm i ${plugins} ${position}`
+    const packageManager = get()
+    if (packageManager !== 'npm') {
+      directive = `${packageManager} add ${plugins} ${position}`
+    }
+    if (existsSync('pnpm-workspace.yaml')) {
+      directive = `pnpm add ${plugins} ${position}w`
+    }
+    return directive
+  }
+  return { get, directive, hasWorkspace }
+}
